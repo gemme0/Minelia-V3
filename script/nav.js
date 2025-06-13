97a3723462c933c8b8a8b94291daffc349425449
gemme0 <137955466+gemme0@users.noreply.github.com>
2025-06-13 22:17:14 +0200

    Add files via upload

---
+++ script/nav.js
@@ -0,0 +1,13 @@
+const toggleButton = document.querySelector(".toggle-button");
+const mainMenu = document.querySelector(".invisible");
+toggleButton.addEventListener('click', function () {
+    // Toggles the active class
+    mainMenu.classList.toggle('active');
+        if (mainMenu.classList.contains("active")) {              
+            // Adds the close (x) icon 
+            toggleButton.innerHTML = "&#215;";
+        } else {          
+            // Adds the hamburger icon 
+            toggleButton.innerHTML = "&#9776;";
+        }
+});
